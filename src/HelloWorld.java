2fc236db249be0e394b0041453e5370777c61cd5
houqi <hou_qi_java@163.com>
2023-04-04 16:04:23 +0800

    hq2

---
+++ src/HelloWorld.java
@@ -0,0 +1,15 @@
+/**
+ * ClassName: HelloWorld
+ * Package: PACKAGE_NAME
+ * Description:
+ *
+ * @Author 后琦
+ * @Create 2023/4/4 16:03
+ * @Version 1.0
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+}
